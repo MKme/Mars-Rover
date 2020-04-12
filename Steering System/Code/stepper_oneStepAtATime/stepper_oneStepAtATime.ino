e0cb0839cb0cab571a3c273894a13b121afbc4af
Eric <MKme@users.noreply.github.com>
2020-04-12 16:24:36 -0400

    Update stepper_oneStepAtATime.ino

--- Steering System/Code/stepper_oneStepAtATime/stepper_oneStepAtATime.ino
+++ Steering System/Code/stepper_oneStepAtATime/stepper_oneStepAtATime.ino
@@ -14,22 +14,22 @@ Stepper myStepper = Stepper(stepsPerRevolution, 8, 9, 10, 11);
 
 void setup() {
   // Set the motor speed (RPMs):
-  myStepper.setSpeed(25); //was originally 100
+  myStepper.setSpeed(3); //was originally 100
   pinMode(13, OUTPUT); // This is stepper enable to stop the hold function which draws ~700mA - just enable hold when moving or turning only will save a lot of power 
 }
 
 void loop() {
   // Step one revolution in one direction:
   digitalWrite(13, HIGH);
-  myStepper.step(200);
-  digitalWrite(13, LOW);
+  myStepper.step(10);
+  //digitalWrite(13, LOW);
 
   delay(2000);
 
   // Step on revolution in the other direction:
   digitalWrite(13, HIGH);
-  myStepper.step(-200);
-  digitalWrite(13, LOW);
+  myStepper.step(-10);
+  //digitalWrite(13, LOW);
 
   delay(2000);
 }
